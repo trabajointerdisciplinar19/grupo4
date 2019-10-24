e29af26a0d88f0956ab8a0f265b604f7fdfc265f
Esejje <38010954+esejjec@users.noreply.github.com>
2019-10-23 21:16:06 -0500

    Create MainActivity.java

---
+++ APP/MainActivity.java
@@ -0,0 +1,10 @@
+package com.example.myapplication2;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
