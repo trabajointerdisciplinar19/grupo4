ca793e23b4e2487ccd86ee7551d39c1c5713f5cf
Esejje <38010954+esejjec@users.noreply.github.com>
2019-10-23 21:57:29 -0500

    Create login.js

---
+++ WEB/views/login.js
@@ -0,0 +1,12 @@
+var mysql = require('mysql');
+var express = require('express');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'nodelogin'
+});
