7ce6ccc816132a3ed8bb44478bb3adf6d747b575
Esejje <38010954+esejjec@users.noreply.github.com>
2019-11-16 09:34:57 -0500

    Create administracion.js

---
+++ WEB/js/administracion.js
@@ -0,0 +1,14 @@
+// Inicializar la base de datos
+var firebaseConfig = {
+    apiKey: "AIzaSyA-u996k8fkyja32jv0kbrb-3-XvtH6yz8",
+    authDomain: "subastatimereal.firebaseapp.com",
+    databaseURL: "https://subastatimereal.firebaseio.com",
+    projectId: "subastatimereal",
+    storageBucket: "subastatimereal.appspot.com",
+    messagingSenderId: "470011455740",
+    //appId: "1:470011455740:web:3d4466ce3f50c43a508f79",
+    //measurementId: "G-1TK0PZJ07Y"
+    };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
