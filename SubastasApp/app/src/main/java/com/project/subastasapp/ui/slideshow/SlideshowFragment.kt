daf0586e3170352ae23ffcd43cdcaaa8847e1ca4
Esejje <38010954+esejjec@users.noreply.github.com>
2019-11-28 00:30:12 -0500

    Update SlideshowFragment.kt

--- SubastasApp/app/src/main/java/com/project/subastasapp/ui/slideshow/SlideshowFragment.kt
+++ SubastasApp/app/src/main/java/com/project/subastasapp/ui/slideshow/SlideshowFragment.kt
@@ -1,5 +1,36 @@
 package com.project.subastasapp.ui.slideshow
 
+
+import android.app.ProgressDialog;
+import android.content.Intent
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore
+//import android.support.annotation.NonNull
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+
+//import com.google.android.gms.OnFailureListener;
+//import com.google.android.gms.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.project.subastasapp.Entidades.User
+import com.project.subastasapp.MainActivity
+import com.project.subastasapp.R
+import kotlinx.android.synthetic.main.activity_login.*
+import java.lang.ref.PhantomReference
+
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +42,7 @@ import androidx.lifecycle.ViewModelProviders
 import com.project.subastasapp.R
 
 class SlideshowFragment : Fragment() {
-
+    
     private lateinit var slideshowViewModel: SlideshowViewModel
 
     override fun onCreateView(
