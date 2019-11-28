d40657bf429656b9f9ff5dd53a6f196063013d86
zulmarina1687 <luzmarinapilco@gmail.com>
2019-11-28 10:50:16 -0500

    Actualizacion Elementos

---
+++ subastas/elements/footer.php
@@ -0,0 +1,135 @@
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color pt-4">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Footer links -->
+    <div class="row text-center text-md-left mt-3 pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3 section_footer">
+        <h6 class="text-uppercase mb-4 font-weight-bold">PUJASS PEEE</h6>
+        <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet,
+          consectetur
+          adipisicing elit.</p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3 section_footer">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Acerca de PujasPe</h6>
+        <p>
+          <a href="#!">¿Que son los Pujas?</a>
+        </p>
+        <p>
+          <a href="#!">¿Quines Somos?</a>
+        </p>
+        <p>
+          <a href="#!">Tipos de Subastas</a>
+        </p>
+        <p>
+          <a href="#!">Testimonios</a>
+        </p>
+      </div>
+      <!-- Grid column -->
+
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3 section_footer">
+        <h6 class="text-uppercase mb-4 font-weight-bold">Servicio al cliente</h6>
+        <p>
+          <a href="#!">YAyuda y Soporte</a>
+        </p>
+        <p>
+          <a href="#!">Como funciona</a>
+        </p>
+        <p>
+          <a href="#!">Preguntas Frecuentes</a>
+        </p>
+        <p>
+          <a href="#!">Contactanos</a>
+        </p>
+
+      </div>
+
+      <!-- Grid column -->
+      <hr class="w-100 clearfix d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3 section_footer" >
+        <h6 class="text-uppercase mb-4 font-weight-bold">Contactanos</h6>
+        <p>
+          <i class="fas fa-home mr-3"></i> Av. Villa el salvador</p>
+        <p>
+          <i class="fas fa-envelope mr-3"></i> soporte@pujas.com</p>
+        <p>
+          <i class="fas fa-phone mr-3"></i> 948373747834</p>
+        <p>
+          <i class="fas fa-print mr-3"></i> 948373747834</p>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Footer links -->
+
+    <hr>
+
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
+
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
+
+        <!--Copyright-->
+        <p class="text-center text-md-left">© 2019 Copyright:
+           <strong> TI2 </strong>
+          
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-5 col-lg-4 ml-lg-0">
+
+        <!-- Social buttons -->
+        <div class="text-center text-md-right">
+          <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-twitter"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-google-plus-g"></i>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+</footer>
+<!-- Footer -->
---
+++ subastas/elements/head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.3.0/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.3.0/firebase-database.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.3.0/firebase.js"></script>
+      <script src="js/conexion.js"></script>
+    <title>Subastas Web</title>
+</head>
