333a9b22887f0b3110b95348cfd856868cc19c3a
Esejje <38010954+esejjec@users.noreply.github.com>
2019-11-28 20:59:32 -0500

    Add files via upload
    header

---
+++ subastas/elements/header.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-light">
+
+  <div id="titulo_pagina">
+		  <div>Subastas Tiempo real</div>
+		 
+		</div>
+  <a class="navbar-brand" style="margin_left:-100px;"> <img src="img/logo_subastas.png" style="width:260px; "></a>
+  <form class="bot_user form-inline" id="botones_user">
+  <!-- <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">-->
+  <a class="btn btn-outline-success my-2 my-sm-0"  class="link_login" href="login.php">Inciar Sesion</a>
+    <a class="btn btn-outline-success my-2 my-sm-0" class="link_registro" href="registro.php">Registrarse</a>
+  </form>
+</nav>
