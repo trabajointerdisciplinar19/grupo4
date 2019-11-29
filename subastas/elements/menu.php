ab68d8814b22b0e08d3815b673daace861d20804
Esejje <38010954+esejjec@users.noreply.github.com>
2019-11-28 20:59:40 -0500

    Add files via upload
    menu

---
+++ subastas/elements/menu.php
@@ -0,0 +1,38 @@
+ <!--<nav class="navbar navbar-inverse navbar-fixed-top">
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="index.html" title="Inicio">Inicio</a></li>
+                <li><a href="administracion.html" title="">Administracion</a></li>
+                <li><a href="nuevoproducto.html" title="">Añadir Productos</a></li>
+            </ul>
+        </div>
+    </nav>-->
+
+<nav class="navbar navbar-expand-lg navbar-light" id="menu">
+  <!--<a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>-->
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Subastas en Vivo <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Proximas Subastas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Probando</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">Categorias </a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="#">Testimonios</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      </li>
+    </ul>
+  </div>
+</nav>
