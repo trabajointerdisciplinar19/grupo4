b2332c9af88a7b24642b80bc3b28904a1a77909c
Esejje <38010954+esejjec@users.noreply.github.com>
2019-12-01 23:12:11 -0500

    Update agregar_producto.php
    add producto

--- subastas/agregar_producto.php
+++ subastas/agregar_producto.php
@@ -1,17 +1,33 @@
-<!DOCTYPE html> 
-<html> 
-<head> 
-    <title> 
-        Async file upload with jQuery 
-    </title> 
-      
-    <script src= 
+<?php include("elements/head.php");?>
+ <script src="js/administracion.js"></script>
+ <script src= 
 "https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"> 
     </script> 
-</head> 
-  
+
+<body>
+    <?php include("elements/menu.php");?>
+<br><br>
+
+<title> 
+        Async file upload with jQuery 
+</title> 
+    
 <body> 
-    <div align="center"> 
+    <div class="container-fluid">
+        <form id="formularioAlta">
+            <div class="form-group">
+                <label for="articulo">Articulo</label>
+                <input type="text" class="form-control" id="articulo" placeholder="Nombre del artículo">
+            </div>
+            <div class="form-group">
+                <label for="descripcion">Descripción</label>
+                <textarea class="form-control" id="descripcion" placeholder="Descripción del artículo" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="precio">Precio</label>
+                <input type="text" class="form-control" id="precio" placeholder="Precio del artículo">
+            </div>
+            <div align="center"> 
         <form method="post" action="" enctype="multipart/form-data"
                 id="myform"> 
    
@@ -21,7 +37,11 @@
                         id="but_upload"> 
             </div> 
         </form> 
-    </div>     
+    </div>  
+        </form>
+    </div>
+
+       
        
     <script type="text/javascript"> 
         $(document).ready(function() { 
@@ -49,6 +69,13 @@
             }); 
         }); 
     </script> 
-</body> 
-  
-</html> 
+
+<br><br>
+<!--<br><br><br><br><br><br><br><br> --->
+ <!-- /.container -->
+
+ <?php include("elements/footer.php");?>
+
+</body>
+</html>
+
