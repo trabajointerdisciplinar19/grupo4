e0f4007a97eb3f379e9a375bff749b859cccd527
Esejje <38010954+esejjec@users.noreply.github.com>
2019-12-01 23:35:31 -0500

    Add files via upload

---
+++ subastas/miperfil.php
@@ -0,0 +1,18 @@
+<?php include("elements/head.php");?>
+ <script src="js/index.js"></script>
+ <script>
+
+</script>
+ 
+<body>
+    <?php include("elements/header.php");?>
+    <?php include("elements/menu.php");?>
+<br><br>
+<div  class="container-fluid" id="listado">  </div>
+
+<br><br>
+
+ <?php include("elements/footer.php");?>
+
+</body>
+</html>
