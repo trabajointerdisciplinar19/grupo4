6005cc6a9131fec5ab4f60706a1a01beeb50744f
zulmarina1687 <luzmarinapilco@gmail.com>
2019-12-05 09:29:58 -0500

    visualizacion tecnologia

--- subastas/tecn.php
+++ subastas/tecn.php
@@ -1,5 +1,5 @@
  <?php include("elements/head.php");?>
- <script src="js/administracion.js"></script>
+ <script src="js/tecnologia.js"></script>
  <script>
 
 </script>
@@ -8,6 +8,17 @@
     <?php include("elements/menu.php");?>
     <?php include("elements/menuC.php");?>
 <br><br>
+
+
+<div class="container">
+  <div class="col-lg-12">  
+    <div class="row" id="listado">
+
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.col-lg-12-->
+</div>
 
 
  <?php include("elements/footer.php");?>
