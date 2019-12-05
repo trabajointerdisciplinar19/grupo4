2844ce710ce10c9f3e6e8dde0d369a6c55671309
Erika Sejje <esejjec@unsa.edu.pe>
2019-12-05 04:38:42 -0500

    Categorias

---
+++ subastas/anti.php
@@ -0,0 +1,16 @@
+ <?php include("elements/head.php");?>
+ <script src="js/administracion.js"></script>
+ <script>
+
+</script>
+
+<body>
+    <?php include("elements/menu.php");?>
+    <?php include("elements/menuC.php");?>
+<br><br>
+
+
+ <?php include("elements/footer.php");?>
+
+</body>
+</html>
---
+++ subastas/carro.php
@@ -0,0 +1,17 @@
+ <?php include("elements/head.php");?>
+ <script src="js/administracion.js"></script>
+ <script>
+
+</script>
+
+<body>
+    <?php include("elements/menu.php");?>
+    <?php include("elements/menuC.php");?>
+<br><br>
+
+
+ <?php include("elements/footer.php");?>
+
+</body>
+</html>
+
