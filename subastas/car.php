0b88f2f3a733d37b3881c763f5e478918b8ef327
Esejje <38010954+esejjec@users.noreply.github.com>
2019-12-07 10:12:32 -0500

    Add files via upload
    views carro

---
+++ subastas/car.php
@@ -0,0 +1,28 @@
+
+ <?php include("elements/head.php");?>
+ <script src="js/carro.js"></script>
+ <script>
+
+</script>
+
+<body>
+    <?php include("elements/menu.php");?>
+    <?php include("elements/menuC.php");?>
+<br><br>
+
+
+<div class="container">
+  <div class="col-lg-12">  
+    <div class="row" id="listado">
+
+    </div>
+    <!-- /.row -->
+  </div>
+  <!-- /.col-lg-12-->
+</div>
+
+
+ <?php include("elements/footer.php");?>
+
+</body>
+</html>
